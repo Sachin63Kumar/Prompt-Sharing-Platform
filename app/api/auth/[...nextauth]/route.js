da3a2c83b5bc2989d970200368a0cc6a3aa0ce87
Sachin_Kumar <202152335@iiitvadodara.ac.in>
2024-03-03 15:12:42 +0530

    Added search feature,view other user profile, search based on tags

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,6 @@
-import NextAuth from "next-auth/next";
+import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+
 import User from "@models/user";
 import { connectToDB } from "@utils/database";
 
@@ -12,24 +13,20 @@ const handler = NextAuth({
   ],
   callbacks: {
     async session({ session }) {
-      const sessionUser = await User.findOne({
-        email: session.user.email,
-      });
-
+      // store the user id from MongoDB to session
+      const sessionUser = await User.findOne({ email: session.user.email });
       session.user.id = sessionUser._id.toString();
 
       return session;
     },
-    async signIn({ profile }) {
+    async signIn({ account, profile, user, credentials }) {
       try {
         await connectToDB();
 
-        // check if a user already exists.
-        const userExists = await User.findOne({
-          email: profile.email,
-        });
+        // check if user already exists
+        const userExists = await User.findOne({ email: profile.email });
 
-        // if not, create a new user
+        // if not, create a new document and save user in MongoDB
         if (!userExists) {
           await User.create({
             email: profile.email,
@@ -37,9 +34,10 @@ const handler = NextAuth({
             image: profile.picture,
           });
         }
+
         return true;
       } catch (error) {
-        console.log(error);
+        console.log("Error checking if user exists: ", error.message);
         return false;
       }
     },
